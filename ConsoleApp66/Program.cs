fe22846b691d64f0c18299c3059ceec430deb3e3
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:43:09 +0330

    Add files via upload

---
+++ ConsoleApp66/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHi("mike", 78);
+            sayHi("john", 34);
+            sayHi("max", 23);
+            Console.ReadLine();
+        }
+
+        static void sayHi(string name,int age)
+        {
+            Console.WriteLine(" Hello " + name + " you are " + age );
+            Console.ReadLine();
+              
+        }
+    }
+}
